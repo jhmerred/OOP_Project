6b8c8d0405f08f7140946cfcd3c44cc9482d8841
seungjae0421 <81350708+seungjae0421@users.noreply.github.com>
2023-11-21 23:19:13 +0900

    Delete NewProject directory

--- NewProject/app/src/main/java/com/example/newproject/dto/UserRegisterDto.kt
+++
@@ -1,9 +0,0 @@
-package com.example.newproject.dto
-
-import com.example.newproject.model.domain.User
-
-class UserRegisterDto(private val name: String, private val username: String, private val password: String, private val userProfileImg: String = "") {
-    fun toEntity(): User {
-        return User(this.name, this.username, this.password, this.userProfileImg)
-    }
-}
--- NewProject/app/src/main/java/com/example/newproject/model/domain/Todo.kt
+++
@@ -1,9 +0,0 @@
-package com.example.newproject.model.domain
-
-import java.util.Date
-
-class Todo(
-    var deadline: Date,
-    var name: String
-) {
-}
