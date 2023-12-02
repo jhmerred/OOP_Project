a75c02bdea1f5d057cdf0ff3d43a7e9d9e476c9d
seungjae0421 <seungjae0421@kau.kr>
2023-12-02 21:13:11 +0900

    <FriendFragment.kt> 1. data class Friend 추가 2. FriendAdapter class 생성: RecyclerView 에 표시할 데이터 뷰 생성, 바인딩(item_friend.xml 사용) 3. FriendFragment 수정 companion object 부분 주석처리(오류) loadFriends() 메서드 구현 friendRecyclerView.layoutManager = LinearLayoutManager(context) 추가 4. android.widget.TextView/ImageView import하였음(TextView, ImageView 클래스 참조를 위해) <item_friend.xml> RecyclerView에 표시할 아이템 레이아웃

--- app/src/main/java/com/example/newproject/view/FriendFragment.kt
+++ app/src/main/java/com/example/newproject/view/FriendFragment.kt
@@ -5,9 +5,38 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.newproject.R
+import android.widget.TextView
+import android.widget.ImageView
+
+data class Friend(val name: String, val profileImage: String)
+class FriendAdapter(private val friendList: List<Friend>) : RecyclerView.Adapter<FriendAdapter.FriendViewHolder>() {
+
+    class FriendViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val friendName: TextView = itemView.findViewById(R.id.friendName)
+        val friendImage: ImageView = itemView.findViewById(R.id.friendImage)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FriendViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_friend, parent, false)
+        return FriendViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: FriendViewHolder, position: Int) {
+        val friend = friendList[position]
+        holder.friendName.text = friend.name
+        // Assuming the profile image is a URL, you may need to use a library such as Glide or Picasso to load it.
+        // Glide.with(holder.itemView.context).load(friend.profileImage).into(holder.friendImage)
+    }
+
+    override fun getItemCount() = friendList.size
+}
 
 class FriendFragment: Fragment() {
+    private lateinit var friendRecyclerView: RecyclerView
+    private lateinit var friendAdapter: FriendAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -19,24 +48,36 @@ class FriendFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_friend, container, false)
+        val view= inflater.inflate(R.layout.fragment_friend,container,false)
+        //Initialize RecyclerView and Adapter
+        friendRecyclerView = view.findViewById(R.id.friendRecyclerView)
+        friendAdapter = FriendAdapter(loadFriends())
+        friendRecyclerView.layoutManager=LinearLayoutManager(context)
+        friendRecyclerView.adapter = friendAdapter
+        return view
+    }
+    private fun loadFriends(): List<Friend>{
+        //Load your friends data here. This is just a placeholder.
+        return listOf(
+            Friend("홍길동", "https://example.com/image1.png")
+        )
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FriendFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FriendFragment().apply {
-                arguments = Bundle().apply {
-                }
-            }
-    }
+//    companion object {
+//        /**
+//         * Use this factory method to create a new instance of
+//         * this fragment using the provided parameters.
+//         *
+//         * @param param1 Parameter 1.
+//         * @param param2 Parameter 2.
+//         * @return A new instance of fragment FriendFragment.
+//         */
+//        // TODO: Rename and change types and number of parameters
+//        @JvmStatic
+//        fun newInstance(param1: String, param2: String) =
+//            FriendFragment().apply {
+//                arguments = Bundle().apply {
+//                }
+//            }
+//    }
 }
