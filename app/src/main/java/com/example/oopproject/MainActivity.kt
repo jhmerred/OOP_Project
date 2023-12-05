97ffbb8247247cca7545985a88257be402f12831
gunapark <ncc0816@gmail.com>
2023-12-05 20:32:04 +0900

    [Feature] ADD-123

--- app/src/main/java/com/example/oopproject/MainActivity.kt
+++ app/src/main/java/com/example/oopproject/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    val w = 123
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
